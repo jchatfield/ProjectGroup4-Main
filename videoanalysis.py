483c386d3b0e6a25c7f4926fa84a1934595209d6
dhouse31 <71714236+dhouse31@users.noreply.github.com>
2020-09-27 17:45:12 -0400

    added mean brightess values, converted code into function with returns for BGRA and brightness values

--- videoanalysis.py
+++ videoanalysis.py
@@ -2,29 +2,36 @@ import ffmpeg
 import numpy as np
 import cv2
 
+#video = directory pathway to video file
+def Analyze(video):
+    #setting up video capture object (webcam right now for testing video data)
+    cap = cv2.VideoCapture(video)
+    #print(cap.isOpened())
 
-#setting up video capture object (webcam right now for testing video data)
-cap = cv2.VideoCapture(0)
-
-
-print(cap.isOpened())
-
-
-#looping through each frame of video
-while(cap.isOpened()):
-    ret, frame = cap.read()
-
-    #Video analysis operations will go here
-    if ret == True:
-        meanbgr = cv2.mean(frame)
-        print(meanbgr)
-        cv2.imshow('frame', frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+    #looping through each frame of video
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        #Video analysis operations will go here
+        #mean BGRA values
+        #mean "brightness" (average of the average grayscale values per frame)
+        if ret == True:
+            meanbgr = cv2.mean(frame)
+            grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            meanbrightness = np.mean(grayscale)
+            #print(meanbrightness)
+            #print(meanbgr)
+            cv2.imshow('frame', frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        else:
             break
-    else:
-        break
+    #releasing captured video data and destroying the window (for testing)
+    cap.release()
+    cv2.destroyAllWindows()
+    print(meanbgr,meanbrightness)
+    return meanbgr, meanbrightness
 
 
-#releasing captured video data and destroying the window (for testing)
-cap.release()
-cv2.destroyAllWindows()
+
+
+meanbgr,meanbrightness = Analyze(0)
