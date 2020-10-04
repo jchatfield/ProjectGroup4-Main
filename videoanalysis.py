b3b97f15cb3fa82ef00e95c7e26a9ebec6483889
dhouse31 <71714236+dhouse31@users.noreply.github.com>
2020-10-04 13:47:30 -0400

    Added mean image entropy value per frame. Created numpy arrays as the method of exchanging info. Functions now returns meanRed,meanBlue,meanGreen,meanBrightness,meanEntropy

--- videoanalysis.py
+++ videoanalysis.py
@@ -4,7 +4,13 @@ import cv2
 
 #video = directory pathway to video file
 def Analyze(video):
-    #setting up video capture object (webcam right now for testing video data)
+    #setting up video capture object and np arrays (webcam right now for testing video data)
+    meanRed = np.array([])
+    meanGreen = np.array([])
+    meanBlue = np.array([])
+    meanEntropy = np.array([])
+    meanBrightness = np.array([])
+
     cap = cv2.VideoCapture(video)
     #print(cap.isOpened())
 
@@ -15,11 +21,20 @@ def Analyze(video):
         #mean BGRA values
         #mean "brightness" (average of the average grayscale values per frame)
         if ret == True:
-            meanbgr = cv2.mean(frame)
+            bgr = cv2.mean(frame)
             grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            meanbrightness = np.mean(grayscale)
-            #print(meanbrightness)
-            #print(meanbgr)
+            brightness = np.mean(grayscale)
+            marg = np.histogramdd(np.ravel(frame), bins = 256)[0]/frame.size
+            marg = list(filter(lambda p: p > 0, np.ravel(marg)))
+            entropy = -np.sum(np.multiply(marg, np.log2(marg)))
+
+            meanRed = np.append(meanRed,bgr[2])
+            meanGreen = np.append(meanRed,bgr[1])
+            meanBlue = np.append(meanRed,bgr[0])
+            meanEntropy = np.append(meanEntropy,entropy)
+            meanBrightness = np.append(meanBrightness,brightness)
+
+
             cv2.imshow('frame', frame)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
@@ -28,9 +43,8 @@ def Analyze(video):
     #releasing captured video data and destroying the window (for testing)
     cap.release()
     cv2.destroyAllWindows()
-    print(meanbgr,meanbrightness)
-    return meanbgr, meanbrightness
-
+    print(meanRed,meanBlue,meanGreen,meanBrightness,meanEntropy)
+    return meanRed,meanBlue,meanGreen,meanBrightness,meanEntropy
 
 
 
