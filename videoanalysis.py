0771ff60658f03d1e730a663072056ce4a2e52e3
dhouse31 <71714236+dhouse31@users.noreply.github.com>
2020-11-24 09:25:30 -0500

    new video analysis file
    only video analysis functions in this file

--- videoanalysis.py
+++ videoanalysis.py
@@ -1,51 +1,35 @@
-import ffmpeg
 import numpy as np
 import cv2
+import math
+import wave
+from scipy import interpolate
 
-#video = directory pathway to video file
-def Analyze(video):
-    #setting up video capture object and np arrays (webcam right now for testing video data)
-    meanRed = np.array([])
-    meanGreen = np.array([])
-    meanBlue = np.array([])
-    meanEntropy = np.array([])
-    meanBrightness = np.array([])
-
-    cap = cv2.VideoCapture(video)
-    #print(cap.isOpened())
-
-    #looping through each frame of video
-    while(cap.isOpened()):
-        ret, frame = cap.read()
-        #Video analysis operations will go here
-        #mean BGRA values
-        #mean "brightness" (average of the average grayscale values per frame)
-        if ret == True:
-            bgr = cv2.mean(frame)
-            grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            brightness = np.mean(grayscale)
-            marg = np.histogramdd(np.ravel(frame), bins = 256)[0]/frame.size
-            marg = list(filter(lambda p: p > 0, np.ravel(marg)))
-            entropy = -np.sum(np.multiply(marg, np.log2(marg)))
-
-            meanRed = np.append(meanRed,bgr[2])
-            meanGreen = np.append(meanRed,bgr[1])
-            meanBlue = np.append(meanRed,bgr[0])
-            meanEntropy = np.append(meanEntropy,entropy)
-            meanBrightness = np.append(meanBrightness,brightness)
+#This function analyzes a single video frame and outputs 5
+#different video features, meanRGB, brightness, and entropy
+#The function will be used once per video frame and then output
+#to the synth function
 
 
-            cv2.imshow('frame', frame)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-        else:
-            break
-    #releasing captured video data and destroying the window (for testing)
-    cap.release()
-    cv2.destroyAllWindows()
-    print(meanRed,meanBlue,meanGreen,meanBrightness,meanEntropy)
+def Analyze(frame):
+    bgr = cv2.mean(frame)
+    grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    brightness = np.mean(grayscale)
+    marg = np.histogramdd(np.ravel(frame), bins = 256)[0]/frame.size
+    marg = list(filter(lambda p: p > 0, np.ravel(marg)))
+    entropy = -np.sum(np.multiply(marg, np.log2(marg)))
+
+    meanRed = bgr[2]
+    meanGreen = bgr[1]
+    meanBlue = bgr[0]
+    meanEntropy = entropy
+    meanBrightness = brightness
     return meanRed,meanBlue,meanGreen,meanBrightness,meanEntropy
 
 
 
-meanbgr,meanbrightness = Analyze(0)
+
+#normalization function given known max and min values and known new max and new min values
+#using to map known values from the video parameters to a different set of values.
+def Normalize(value,min,max,newmin,newmax):
+    normalized = (newmax-newmin)/(max-min)*(value-max) + newmax
+    return normalized
